de19a4e0ef18f68901a15f7c08e00e4023bdc1ff
kexplx <oscar.rosner@web.de>
2020-05-26 18:12:45 +0200

    feat: add SolveBoxComponent

---
+++ ng-frontend/src/app/mail-generate/components/mail-generation-overview/solve-box/solve-box.component.ts
@@ -0,0 +1,12 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-solve-box',
+  templateUrl: './solve-box.component.html',
+  styleUrls: ['./solve-box.component.scss'],
+})
+export class SolveBoxComponent implements OnInit {
+  constructor() {}
+
+  ngOnInit() {}
+}
