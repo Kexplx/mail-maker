f9f4a6bf4f690b8e336b9f7fe8ba337c6e66f6c1
kexplx <oscar.rosner@web.de>
2020-05-26 17:41:52 +0200

    feat: add routes for lazy loaded feature module MailGenerateModule

---
+++ ng-frontend/src/app/app-routing.module.ts
@@ -0,0 +1,22 @@
+import { NgModule } from '@angular/core';
+
+import { Routes, RouterModule } from '@angular/router';
+
+const routes: Routes = [
+  {
+    path: '',
+    pathMatch: 'full',
+    redirectTo: 'generate',
+  },
+  {
+    path: 'generate',
+    loadChildren: () =>
+      import('./mail-generate/mail-generate.module').then(m => m.MailGenerateModule),
+  },
+];
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule],
+})
+export class AppRoutingModule {}
