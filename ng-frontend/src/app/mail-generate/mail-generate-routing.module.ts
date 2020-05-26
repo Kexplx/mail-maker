71d7b9acfc09ef6e435984af58c364f0756255a6
kexplx <oscar.rosner@web.de>
2020-05-26 18:13:49 +0200

    feat: change routed component to MailGenerationOverviewComponent

--- ng-frontend/src/app/mail-generate/mail-generate-routing.module.ts
+++ ng-frontend/src/app/mail-generate/mail-generate-routing.module.ts
@@ -1,8 +1,8 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
-import { MailGenerationFormComponent } from './components/mail-generation-form/mail-generation-form.component';
+import { MailGenerationOverviewComponent } from './components/mail-generation-overview/mail-generation-overview.component';
 
-const routes: Routes = [{ path: '', component: MailGenerationFormComponent }];
+const routes: Routes = [{ path: '', component: MailGenerationOverviewComponent }];
 
 @NgModule({
   imports: [RouterModule.forChild(routes)],
