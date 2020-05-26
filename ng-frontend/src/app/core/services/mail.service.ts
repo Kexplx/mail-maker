0fa9761e3c1af23cfc008cac2846050f055e883c
kexplx <oscar.rosner@web.de>
2020-05-26 17:57:09 +0200

    doc: add docs for crawl and answer methods

--- ng-frontend/src/app/core/services/mail.service.ts
+++ ng-frontend/src/app/core/services/mail.service.ts
@@ -5,6 +5,7 @@ import { CaptchaTask } from './models/captcha-task.model';
 import { throwError, Observable } from 'rxjs';
 import { tap, catchError } from 'rxjs/operators';
 import { Result } from './models/result.model';
+import { Answer } from './models/answer.model';
 
 const urls = {
   development: 'http://localhost:3000',
@@ -19,19 +20,34 @@ const compileTimeSwitchedUrl = environment.production ? urls.production : urls.d
 export class MailService {
   constructor(private http: HttpClient) {}
 
+  /**
+   * Makes a GET request to the backend to obtain a @link(CaptchaTask)
+   *
+   * Logs any received data inside tap() operator
+   * Re-throws any errors that occur inside catchError() operator
+   */
   crawl(): Observable<CaptchaTask> {
-    return this.http.get<CaptchaTask>(`${compileTimeSwitchedUrl}/crawl`).pipe(
+    const url = `${compileTimeSwitchedUrl}/crawl`;
+
+    return this.http.get<CaptchaTask>(url).pipe(
       tap(data => console.log(`Received data from /crawl endpoint after GET: ${data}`)),
       catchError(err => throwError(`Error at /crawl endpoint after GET: ${err}`)),
     );
   }
 
-  answer(answer: string): Observable<Result> {
-    return this.http
-      .post<Result>(`${compileTimeSwitchedUrl}/answer`, { answer })
-      .pipe(
-        tap(data => console.log(`Received data from /answer endpoint after POST: ${data}`)),
-        catchError(err => throwError(`Error at /answer endpoint after POST: ${err}`)),
-      );
+  /**
+   * Makes a POST request to the backend to obtain a @link(Result)
+   *
+   * Includes the answer in the POST body
+   *
+   * @param answer An answer containing the given id and captchaAnswer
+   */
+  answer(answer: Answer): Observable<Result> {
+    const url = `${compileTimeSwitchedUrl}/answer`;
+
+    return this.http.post<Result>(url, answer).pipe(
+      tap(data => console.log(`Received data from /answer endpoint after POST: ${data}`)),
+      catchError(err => throwError(`Error at /answer endpoint after POST: ${err}`)),
+    );
   }
 }
