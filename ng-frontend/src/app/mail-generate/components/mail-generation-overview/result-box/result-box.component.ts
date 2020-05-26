22a5cc42b6c49415b7a8b1fe19099b9709403f1c
kexplx <oscar.rosner@web.de>
2020-05-26 18:12:54 +0200

    feat: add ResultBoxComponent

---
+++ ng-frontend/src/app/mail-generate/components/mail-generation-overview/result-box/result-box.component.ts
@@ -0,0 +1,12 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-result-box',
+  templateUrl: './result-box.component.html',
+  styleUrls: ['./result-box.component.scss'],
+})
+export class ResultBoxComponent implements OnInit {
+  constructor() {}
+
+  ngOnInit() {}
+}
