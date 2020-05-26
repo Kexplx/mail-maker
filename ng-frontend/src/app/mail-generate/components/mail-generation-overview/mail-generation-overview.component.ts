3c2045a6427204bf5b72874ab5ea15d61046ac57
kexplx <oscar.rosner@web.de>
2020-05-26 18:13:25 +0200

    feat: add MailGenerationOverviewComponent

---
+++ ng-frontend/src/app/mail-generate/components/mail-generation-overview/mail-generation-overview.component.ts
@@ -0,0 +1,12 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-mail-generation-overview',
+  templateUrl: './mail-generation-overview.component.html',
+  styleUrls: ['./mail-generation-overview.component.scss'],
+})
+export class MailGenerationOverviewComponent implements OnInit {
+  constructor() {}
+
+  ngOnInit() {}
+}
