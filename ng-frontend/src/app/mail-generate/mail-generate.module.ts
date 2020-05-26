c8b71ee7710e8255ac4c6327972762f6acfcf5b8
kexplx <oscar.rosner@web.de>
2020-05-26 18:14:26 +0200

    feat: declare new components

--- ng-frontend/src/app/mail-generate/mail-generate.module.ts
+++ ng-frontend/src/app/mail-generate/mail-generate.module.ts
@@ -2,12 +2,20 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
 import { MailGenerateRoutingModule } from './mail-generate-routing.module';
-import { MailGenerationFormComponent } from './components/mail-generation-form/mail-generation-form.component';
 import { SharedModule } from '../shared/shared.module';
 import { FormsModule } from '@angular/forms';
+import { CrawlBoxComponent } from './components/mail-generation-overview/crawl-box/crawl-box.component';
+import { ResultBoxComponent } from './components/mail-generation-overview/result-box/result-box.component';
+import { SolveBoxComponent } from './components/mail-generation-overview/solve-box/solve-box.component';
+import { MailGenerationOverviewComponent } from './components/mail-generation-overview/mail-generation-overview.component';
 
 @NgModule({
-  declarations: [MailGenerationFormComponent],
+  declarations: [
+    CrawlBoxComponent,
+    ResultBoxComponent,
+    SolveBoxComponent,
+    MailGenerationOverviewComponent,
+  ],
   imports: [CommonModule, FormsModule, MailGenerateRoutingModule, SharedModule],
 })
 export class MailGenerateModule {}
