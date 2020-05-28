e47872d01053a3404bc2e9acd6a4dcf5b99e3091
kexplx <oscar.rosner@web.de>
2020-05-28 23:51:11 +0200

    feat: add AbstractPageHandler

---
+++ nest-backend/src/handler/abstract-page-handler.ts
@@ -0,0 +1,6 @@
+import { Page } from 'puppeteer';
+import { Result } from 'src/models/result.model';
+import { RandomStringGenerator } from 'src/helpers/random-string-generator';
+
+export abstract class AbstractPageHandler {
+}
