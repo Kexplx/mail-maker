420bba73ca1569b78c93ed138534a4e854945623
kexplx <oscar.rosner@web.de>
2020-05-28 23:54:39 +0200

    feat: implements getCaptcha link

--- nest-backend/src/handler/webde-page-handler.ts
+++ nest-backend/src/handler/webde-page-handler.ts
@@ -2,4 +2,15 @@ import { AbstractPageHandler } from './abstract-page-handler';
 import { Result } from 'src/models/result.model';
 
 export class WebdePageHandler extends AbstractPageHandler {
+  private readonly url = 'https://registrierung.web.de/';
+
+  async getCaptchaLink(): Promise<string> {
+    await this.page.goto(this.url);
+    await this.page.waitForNavigation();
+
+    const captchaImg = await this.page.$('#captchaImage');
+    const captchaLink = await this.page.evaluate((element: HTMLImageElement) => element.src, captchaImg);
+
+    return captchaLink;
+  }
 }
