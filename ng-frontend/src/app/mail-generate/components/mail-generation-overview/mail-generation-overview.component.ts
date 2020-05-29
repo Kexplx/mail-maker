f6b716b22d326f8712d09c480bbc6b98876f403d
kexplx <oscar.rosner@web.de>
2020-05-29 07:27:34 +0200

    feat: sets title to MailMaker / Make on init

--- ng-frontend/src/app/mail-generate/components/mail-generation-overview/mail-generation-overview.component.ts
+++ ng-frontend/src/app/mail-generate/components/mail-generation-overview/mail-generation-overview.component.ts
@@ -1,12 +1,17 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, AfterViewInit } from '@angular/core';
+import { Title } from '@angular/platform-browser';
 
 @Component({
   selector: 'app-mail-generation-overview',
   templateUrl: './mail-generation-overview.component.html',
   styleUrls: ['./mail-generation-overview.component.scss'],
 })
-export class MailGenerationOverviewComponent implements OnInit {
-  constructor() {}
+export class MailGenerationOverviewComponent implements OnInit, AfterViewInit {
+  constructor(private titleService: Title) {}
+
+  ngOnInit() {
+    this.titleService.setTitle('MailMaker / Make');
+  }
 
   ngAfterViewInit() {
     const layout = document.querySelector('app-mail-generation-overview .layout') as HTMLDivElement;
