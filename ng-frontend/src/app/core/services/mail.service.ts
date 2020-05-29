804828f19f61f04b5d741c9906ace2e3a5ee72dc
kexplx <oscar.rosner@web.de>
2020-05-29 22:27:48 +0200

    refactor: fixes imports

--- ng-frontend/src/app/core/services/mail.service.ts
+++ ng-frontend/src/app/core/services/mail.service.ts
@@ -1,11 +1,11 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { environment } from 'src/environments/environment';
-import { CaptchaTask } from './models/captcha-task.model';
 import { throwError, Observable } from 'rxjs';
 import { tap, catchError } from 'rxjs/operators';
-import { Result } from './models/result.model';
-import { Answer } from './models/answer.model';
+import { Task } from '../models/task.model';
+import { Answer } from '../models/answer.model';
+import { Result } from '../models/result.model';
 
 const urls = {
   development: 'http://localhost:3000',
@@ -20,28 +20,15 @@ const compileTimeSwitchedUrl = environment.production ? urls.production : urls.d
 export class MailService {
   constructor(private http: HttpClient) {}
 
-  /**
-   * Makes a GET request to the backend to obtain a @link(CaptchaTask)
-   *
-   * Logs any received data inside tap() operator
-   * Re-throws any errors that occur inside catchError() operator
-   */
-  crawl(): Observable<CaptchaTask> {
+  crawl(): Observable<Task> {
     const url = `${compileTimeSwitchedUrl}/crawl`;
 
-    return this.http.get<CaptchaTask>(url).pipe(
+    return this.http.get<Task>(url).pipe(
       tap(data => console.log(`Received data from /crawl endpoint after GET: ${data}`)),
       catchError(err => throwError(`Error at /crawl endpoint after GET: ${err}`)),
     );
   }
 
-  /**
-   * Makes a POST request to the backend to obtain a @link(Result)
-   *
-   * Includes the answer in the POST body
-   *
-   * @param answer An answer containing the given id and captchaAnswer
-   */
   answer(answer: Answer): Observable<Result> {
     const url = `${compileTimeSwitchedUrl}/answer`;
 
