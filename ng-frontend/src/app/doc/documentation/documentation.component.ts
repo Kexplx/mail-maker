14bab0f853088a4a9abd76c9467bef437986abcc
kexplx <oscar.rosner@web.de>
2020-05-29 07:28:34 +0200

    feat: sets title to MailMaker / Docs

---
+++ ng-frontend/src/app/doc/documentation/documentation.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+import { Title } from '@angular/platform-browser';
+
+@Component({
+  selector: 'app-documentation',
+  templateUrl: './documentation.component.html',
+  styleUrls: ['./documentation.component.scss'],
+})
+export class DocumentationComponent implements OnInit {
+  constructor(private titleService: Title) {}
+
+  ngOnInit(): void {
+    this.titleService.setTitle('MailMaker / Docs');
+  }
+}
