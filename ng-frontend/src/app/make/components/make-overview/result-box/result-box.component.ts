475a0b3cbee4d28ed396da9d66805a991be49e6e
kexplx <oscar.rosner@web.de>
2020-05-30 04:46:19 +0200

    feat: implements copyToClipboard method

--- ng-frontend/src/app/make/components/make-overview/result-box/result-box.component.ts
+++ ng-frontend/src/app/make/components/make-overview/result-box/result-box.component.ts
@@ -5,8 +5,14 @@ import { Component, OnInit } from '@angular/core';
   templateUrl: './result-box.component.html',
   styleUrls: ['./result-box.component.scss'],
 })
-export class ResultBoxComponent implements OnInit {
-  constructor() {}
+  copyToClipboard(text: string) {
+    const input = document.createElement('input');
+    input.setAttribute('value', text);
+    document.body.appendChild(input);
+    input.select();
+    document.execCommand('copy');
+    document.body.removeChild(input);
+  }
 
   ngOnInit() {}
 }
