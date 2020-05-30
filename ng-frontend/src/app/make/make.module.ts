772a6829aec91952652b30aa824a1c1baf91f6d0
Oscar Rosner <oscar.rosner@web.de>
2020-05-30 22:07:16 +0200

    feat: add core module

--- ng-frontend/src/app/make/make.module.ts
+++ ng-frontend/src/app/make/make.module.ts
@@ -8,10 +8,17 @@ import { ResultBoxComponent } from './components/make-overview/result-box/result
 import { SolveBoxComponent } from './components/make-overview/solve-box/solve-box.component';
 import { MakeOverviewComponent } from './components/make-overview/make-overview.component';
 import { MakeRoutingModule } from './make-routing.module';
+import { CoreModule } from '../core/core.module';
 
 @NgModule({
   declarations: [CrawlBoxComponent, ResultBoxComponent, SolveBoxComponent, MakeOverviewComponent],
-  imports: [CommonModule, FormsModule, MakeRoutingModule, SharedModule],
+  imports: [
+    CommonModule,
+    FormsModule,
     ReactiveFormsModule,
+    CoreModule,
+    MakeRoutingModule,
+    SharedModule,
+  ],
 })
 export class MakeModule {}
