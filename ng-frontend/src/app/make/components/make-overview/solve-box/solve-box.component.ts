47bc4f84a3ef1a30630520d2402ecc1d9ea65b31
Oscar Rosner <oscar.rosner@web.de>
2020-06-08 10:02:33 +0200

    feat: select task$ and answerLoading$ from state

--- ng-frontend/src/app/make/components/make-overview/solve-box/solve-box.component.ts
+++ ng-frontend/src/app/make/components/make-overview/solve-box/solve-box.component.ts
@@ -6,6 +6,8 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./solve-box.component.scss'],
 })
 export class SolveBoxComponent implements OnInit {
+  @Select(AppState.task) task$: Observable<Task>;
+  @Select(AppState.answerLoading) answerLoading$: Observable<boolean>;
   captchaValidator({ value }: AbstractControl) {
     const captchaRegex = /^[0-9a-zA-Z]{4,6}$/;
 
