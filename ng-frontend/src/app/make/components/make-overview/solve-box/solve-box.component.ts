de08ec586530ae14465723af5a4d02d8368c7d5a
Oscar Rosner <oscar.rosner@web.de>
2020-06-08 10:03:45 +0200

    feat: sanitize captchaLink url

--- ng-frontend/src/app/make/components/make-overview/solve-box/solve-box.component.ts
+++ ng-frontend/src/app/make/components/make-overview/solve-box/solve-box.component.ts
@@ -1,4 +1,11 @@
 import { Component, OnInit } from '@angular/core';
+import { Select, Store } from '@ngxs/store';
+import { AppState } from 'src/app/core/ngxs/app.state';
+import { Observable } from 'rxjs';
+import { Task } from 'src/app/core/models/task.model';
+import { FormControl, Validators, AbstractControl } from '@angular/forms';
+import { ProvideAnswer } from 'src/app/core/ngxs/app.actions';
+import { DomSanitizer, SafeUrl } from '@angular/platform-browser';
 
 @Component({
   selector: 'app-solve-box',
@@ -8,6 +15,17 @@ import { Component, OnInit } from '@angular/core';
 export class SolveBoxComponent implements OnInit {
   @Select(AppState.task) task$: Observable<Task>;
   @Select(AppState.answerLoading) answerLoading$: Observable<boolean>;
+  captcha = new FormControl(null, [this.captchaValidator, Validators.required]);
+  captchaLink: SafeUrl;
+
+  constructor(private store: Store, private sanitizer: DomSanitizer) {}
+
+  ngOnInit() {
+    this.task$.subscribe(task => {
+      this.captchaLink = this.sanitizer.bypassSecurityTrustUrl(task?.captchaLink);
+    });
+  }
+
   captchaValidator({ value }: AbstractControl) {
     const captchaRegex = /^[0-9a-zA-Z]{4,6}$/;
 
@@ -18,5 +36,7 @@ export class SolveBoxComponent implements OnInit {
     return null;
   }
 
-  ngOnInit() {}
+  onSubmit() {
+    this.store.dispatch(new ProvideAnswer(this.captcha.value));
+  }
 }
