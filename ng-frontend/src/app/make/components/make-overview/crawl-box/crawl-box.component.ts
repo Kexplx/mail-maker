4f43b39835bdb45de633bf44f8c3f13c43b06def
Oscar Rosner <oscar.rosner@web.de>
2020-06-08 10:05:53 +0200

    feat: set default provider to webde

--- ng-frontend/src/app/make/components/make-overview/crawl-box/crawl-box.component.ts
+++ ng-frontend/src/app/make/components/make-overview/crawl-box/crawl-box.component.ts
@@ -1,4 +1,9 @@
 import { Component, OnInit } from '@angular/core';
+import { Provider } from 'src/app/core/models/enums/provider.enum';
+import { Store, Select } from '@ngxs/store';
+import { Crawl } from 'src/app/core/ngxs/app.actions';
+import { AppState } from 'src/app/core/ngxs/app.state';
+import { Observable } from 'rxjs';
 
 @Component({
   selector: 'app-crawl-box',
@@ -6,7 +11,18 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./crawl-box.component.scss'],
 })
 export class CrawlBoxComponent implements OnInit {
-  constructor() {}
+  @Select(AppState.crawlLoading) crawlLoading$: Observable<boolean>;
+  @Select(AppState.crawlFailed) crawlFailed$: Observable<boolean>;
 
-  ngOnInit() {}
+  selectedProvider: Provider;
+
+  constructor(private store: Store) {}
+
+  ngOnInit() {
+    this.selectedProvider = Provider.WEBDE;
+  }
+
+  onSubmit() {
+    this.store.dispatch(new Crawl(this.selectedProvider));
+  }
 }
